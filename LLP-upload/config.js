f97c7b76e606f5d3c33eeca85c95df88b5683d68
pervizkenan2-png <pervizkenan2@gmail.com>
2026-03-25 23:05:09 +0100

    frontend login redirect fix

---
+++ LLP-upload/config.js
@@ -0,0 +1,2 @@
+window.API_BASE = "https://rental-backend-tqr6.onrender.com/api/tenants";
+window.TENANTS_URL = window.API_BASE;
---
+++ config.js
@@ -0,0 +1,3 @@
+window.API_BASE = "https://rental-backend-tqr6.onrender.com/api/tenants";
+window.AUTH_BASE = "https://rental-backend-tqr6.onrender.com/api/auth";
+window.TENANTS_URL = window.API_BASE;
