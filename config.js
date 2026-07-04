703923cbc1795ac630b99f3eb05f8fe00edbac80
pervizkenan2-png <pervizkenan2@gmail.com>
2026-07-04 18:24:45 +0200

    Update API URL

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
-window.API_BASE = "https://rental-backend-tqr6.onrender.com";
+window.API_BASE = "https://api.landlord-lite.de";
 
 window.TENANTS_URL = window.API_BASE + "/api/tenants";
 window.REPAIRS_URL = window.API_BASE + "/api/repairs";
 window.AUTH_BASE = window.API_BASE + "/api/auth";
-window.RENT_INCREASE_HISTORY_URL = window.API_BASE + "/api/rent-increase-history";
+window.RENT_INCREASE_HISTORY_URL = window.API_BASE + "/api/rent-increase-history";SE + "/api/rent-increase-history";
